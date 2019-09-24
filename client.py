88315e09b563122cf6f7ee7f4f52559ec25c4a49
cs144 <foo@bar.com>
2019-09-24 13:15:30 -0700

    edited

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ port = int(sys.argv[2])
 path = sys.argv[3]
 
 s.connect((ip,port)) # Connect
-s.send("GET " + path + " HTTP/1.0\n\n") # Send request
+s.send("GET " + path + " HTTP/1.1\n\n") # Send request
 data = s.recv(10000) # Get response
 print(data)
 s.close()
